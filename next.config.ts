2cb45340b59847d10ce66ee52418444053753135
Daniel Hashmi <danialhashmi418@gmail.com>
2024-12-17 11:52:49 +0500

    Added PWA

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,31 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+//   /* other config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+// Initialize the PWA configuration
+const withPWA = withPWAInit({
+  dest: "public", // Destination for service worker and related files
+  // register: true, // Automatically register the service worker
+  // skipWaiting: true, // Skip waiting and activate the service worker immediately
+});
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com"], // Add your allowed domains here
   },
-  /* other config options here */
+  // Add other Next.js config options if needed
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
+
