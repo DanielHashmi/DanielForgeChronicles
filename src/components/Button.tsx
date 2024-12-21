97a75b95e52b77f57f2f0382ffd958faffec0402
Daniel Hashmi <danialhashmi418@gmail.com>
2024-12-21 09:23:25 +0500

    Fixed Merge Errors

--- src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-const Button = (props: { text: string }) => {
-    return (
-        <button className="rounded-full text-nowrap smooth hover:bg-transparent hover:scale-105 px-6 p-2 bg-background dark:bg-[#292a2b] w-fit shadow-[0_0_7px_6px_#02020208]">{props.text}</button>
-
-    )
-}
-
-export default Button
